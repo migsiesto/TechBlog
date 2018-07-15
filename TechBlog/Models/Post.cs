570d7285509ef28c84e7103179b06c3f9d10fa2b
Miguel Siesto <migsiesto@gmail.com>
2018-07-16 00:26:21 +0100

    v0.1 Model and homeController creation

---
+++ TechBlog/Models/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechBlog.Models
+{
+    public class Post
+    {
+        public virtual int Id
+        { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 1)]
+        public virtual string Title
+        { get; set; }
+
+        [Required]
+        [StringLength(150, MinimumLength = 1)]
+        public virtual string Text
+        { get; set; }
+
+        public virtual bool IsPublished
+        { get; set; }
+
+        public virtual DateTime PostedOn
+        { get; set; }
+
+        public virtual DateTime? Modified
+        { get; set; }
+    }
+}
